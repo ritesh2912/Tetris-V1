df5730f6e8a69aa90e0aca6e42abd800d002c561
ritesh2912 <94890713+ritesh2912@users.noreply.github.com>
2023-12-19 13:26:58 +0530

    Update Main.tf

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -61,7 +61,7 @@ resource "aws_security_group" "Jenkins-sg" {
 resource "aws_instance" "web" {
   ami                    = "ami-0287a05f0ef0e9d9a"
   instance_type          = "t2.medium"
-  key_name               = "devsecopsbucket1"
+  key_name               = "Helmkey"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
   iam_instance_profile   = aws_iam_instance_profile.example_profile.name
